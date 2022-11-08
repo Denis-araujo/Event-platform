7aa691bc088848ea09fe966694b2cb314ebac237
Denis Gomes <denisgomes938@gmail.com>
2022-11-08 00:27:55 -0300

    Aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
 import { Header } from "./components/Header/Header"
 import { Lesson } from "./components/Lesson/Lesson"
 import { SideBar } from "./components/SideBar/SideBar"
 import { Video } from "./components/Video/Video"
+import { client } from "./lib/apollo"
 import { Event } from "./pages/Event"
+import { Router } from "./Router"
 
 function App() {
   return (
-    <div>
-      <Event />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
